8afc6deb3b46b2a72a1d812cdbbf6a46402c0c41
despagne.h <despagne.h@e7a5ff3e-4780-d6ab-95ec-48a3ea2572d4>
2011-10-21 09:30:21 +0000

    Initial Commit

---
+++ src/Controller/Controller.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public interface Controller {
+	public void update();
+}
---
+++ src/Model/MoteurImpl.java
@@ -0,0 +1,6 @@
+package Model;
+
+public interface MoteurImpl {
+	public void attach(ObserverMoteur o);
+	public void detach(ObserverMoteur o);
+}
