6e0cff7919b329f2fa4cf1b1abeb98b2618fc6fa
despagne.h@gmail.com <despagne.h@gmail.com@e7a5ff3e-4780-d6ab-95ec-48a3ea2572d4>
2011-12-31 21:42:17 +0000

    doc

--- trunk/src/View/SimulMateriel.java
+++ trunk/src/View/SimulMateriel.java
@@ -1,15 +1,47 @@
 package View;
 
+/**
+ * classe permettant de simuler le materiel conformement 
+ * a l'interface fournis par le constructeur
+ * @author Derive
+ *
+ */
 public interface SimulMateriel {
 	/**
 	 * retourne la position du slider
 	 * @return
 	 */
 	float getPositionSlider();
+	
+	/**
+	 * emet un clic
+	 */
 	void emettreClic();
+	
+	/**
+	 * affiche le texte donne en parametre sur l'afficheur
+	 * du metronome
+	 * @param string texte a afficher
+	 */
 	void setAfficheur(String string);
+	
+	/**
+	 * retourne l'etat d'un bouton
+	 * @param b numero du bouton a tester 1 start, 2 stop, 3 in, 4 dec
+	 * @return true si le bouton est enfonce, false sinon
+	 */
 	boolean getEtatBouton(int b);
+	
+	/**
+	 * allume la led dont le numero est passe en parametre
+	 * @param num 1: led temps, 2: led mesures
+	 */
 	void allumerLED(int num);
+	
+	/**
+	 * eteint la led dont le numero est passe en parametre
+	 * @param num 1: led temps, 2: led mesures
+	 */
 	void eteindreLED(int num);
 
 }
